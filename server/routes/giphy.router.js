23a1658d1c22877035e516b41916ef4a701af8fc
Jacob Motes <jmotes2583@gmail.com>
2021-06-18 11:16:37 -0500

    new pull

--- server/routes/giphy.router.js
+++ server/routes/giphy.router.js
@@ -9,9 +9,9 @@ const router = express.Router();
 router.get('/', (req, res) => {
     console.log('req.query', req.query);
     // console.log('req.body', req.body.search);
-    axios.get(`http://api.giphy.com/v1/gifs/search?api_key=${process.env.GIPHY_API_KEY}&q=${req.query}&limit=2`)
+    axios.get(`http://api.giphy.com/v1/gifs/search?api_key=${process.env.GIPHY_API_KEY}&q=${req.query.query}&limit=2`)
     .then(response => {
-    //   console.log(response.data);
+      console.log(response.data);
       res.send(response.data);
     })
     .catch(err => {

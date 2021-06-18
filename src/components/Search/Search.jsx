318468729d2c407b43f81a63625cb9ff4e333c46
Jacob Motes <jmotes2583@gmail.com>
2021-06-18 10:01:50 -0500

    pulling down

--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -47,7 +47,7 @@ function Search() {
             <form onSubmit={handleSubmit}>
             <input onChange={handleSearchChange} placeholder="search" value={search} />
             <button type="submit">Search</button>
-            <img src = {giphySearch?.data?.[0].images?.original.url} alt="" />
+            {/* <img src = {giphySearch?.data?.[0].images?.original.url} alt="" /> */}
             </form>
 
             <div>
@@ -58,7 +58,7 @@ function Search() {
                             
                             <div key={i} >
                                 <img src = {gif?.images?.original.url} alt="yikes" />
-                                // on click
+                                {/* on click */}
                             </div>
 
                         )

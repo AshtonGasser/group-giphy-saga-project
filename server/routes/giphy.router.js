412f91fb9c795e8ed7116b15a9725872a4350e0b
Olivia Finster <olivia.finster@gmail.com>
2021-06-18 10:44:58 -0500

    limit working

--- server/routes/giphy.router.js
+++ server/routes/giphy.router.js
@@ -9,7 +9,7 @@ const router = express.Router();
 router.get('/', (req, res) => {
     console.log('req.query', req.query);
     // console.log('req.body', req.body.search);
-    axios.get(`http://api.giphy.com/v1/gifs/search?api_key=${process.env.GIPHY_API_KEY}&q=${req.query}`)
+    axios.get(`http://api.giphy.com/v1/gifs/search?api_key=${process.env.GIPHY_API_KEY}&q=${req.query}&limit=2`)
     .then(response => {
     //   console.log(response.data);
       res.send(response.data);
